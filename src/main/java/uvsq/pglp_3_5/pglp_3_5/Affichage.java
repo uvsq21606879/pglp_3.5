60f1403a152bc4098f0aa86e52929118bebc09e9
root <said.mohammed-seghir@ens.uvsq.fr>
2020-03-17 13:13:29 +0100

    Crétion des classes

---
+++ src/main/java/uvsq/pglp_3_5/pglp_3_5/Affichage.java
@@ -0,0 +1,9 @@
+package uvsq.pglp_3_5.pglp_3_5;
+
+public interface Affichage {
+
+	void affichageTemporel(String descriptif);
+	
+	void affichageTemporelSLF4J(String descriptif);
+
+}
---
+++ src/main/java/uvsq/pglp_3_5/pglp_3_5/Affilog.java
@@ -0,0 +1,27 @@
+package uvsq.pglp_3_5.pglp_3_5;
+
+import java.time.LocalDateTime;
+import java.util.logging.Logger;
+
+
+import org.slf4j.LoggerFactory;
+
+public class Affilog implements Affichage {
+
+	final org.slf4j.Logger logger;
+	
+	public Affilog() {
+		
+		this.logger = LoggerFactory.getLogger(Affilog.class);
+		
+		
+	}
+	public void affichageTemporel(final String descriptif) {
+		System.out.println(LocalDateTime.now() + descriptif);
+	}
+	
+	public void affichageTemporelSLF4J(final String descriptif) {
+		logger.info(LocalDateTime.now() + descriptif);
+	}
+
+}
